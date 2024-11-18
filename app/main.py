108010f615a67f7239dba18d35ba218c0c0bc7c4
bengarozzo <157422827+bengarozzo@users.noreply.github.com>
2024-11-18 15:14:38 -0500

    spotify

--- app/main.py
+++ app/main.py
@@ -3,17 +3,101 @@
 from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
-import mysql.connector
-from mysql.connector import Error
 import json
 import os
+import mysql.connector
+from mysql.connector import Error
 
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+DBHOST = "ds2022.cqee4iwdcaph.us-east-1.rds.amazonaws.com"
+DBUSER = "admin"
+DBPASS = os.getenv('DBPASS')
+DB = "huk5pd"
+
+db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
+cur=db.cursor()
+
+
+@app.get('/genres')
+def get_genres():
+    query = "SELECT * FROM genres ORDER BY genreid;"
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        return(json_data)
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
+    
+
+@app.get('/songs')
+def get_songs():
+    query = "Select * From songs ORDER BY id;"
+    try:
+        cur.execute(query)
+        headers = [x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data = []
+        for result in results:
+            json_data.append(dict(zip(headers, result)))
+        return json_data
+    except Error as e:
+        print("MySQL Error: ", str(e))
+        return {"Error": "MySQL Error: " + str(e)}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@app.get("/hoos")
+def greet():
+    return {"message": "go hoos!"}
+
 
 @app.get("/")  # zone apex
 def zone_apex():
-    return {"Hello": "Hello API"}
+    return {"Good Day Sunshine": "Ben!"}
 
 @app.get("/add/{a}/{b}")
 def add(a: int, b: int):
     return {"sum": a + b}
+
+@app.get("/square/{a}")
+def square(a: int):
+    return {"square": a * a}
+
+@app.get("/multiply/{c}/{d}")
+def multiply(c:int, d: int):
+        return{"product": c * d}
+
+@app.get("/goodbye")
+def say_goodbye():
+    return {"message": "Goodbye!"}
